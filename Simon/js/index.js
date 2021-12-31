e4eccba7ba6096791a4f5f0082e00fb73a06846d
Stan2hssn <86244342+Stan2hssn@users.noreply.github.com>
2021-12-31 18:57:43 +0100

    31 DEC 21

---
+++ Simon/js/index.js
@@ -0,0 +1,228 @@
+let order = [];
+let playerOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let strict = true;
+let noise = true;
+let on = false;
+let win;
+
+const turnCounter = document.querySelector("#turn");
+const box1 = document.querySelector("#box1");
+const box2 = document.querySelector("#box2");
+const box3 = document.querySelector("#box3");
+const box4 = document.querySelector("#box4");
+const box5 = document.querySelector(".box5");
+const box6 = document.querySelector(".box6");
+const box7 = document.querySelector(".box7");
+const box8 = document.querySelector(".box8");
+const box9 = document.querySelector(".box9");
+const box10 = document.querySelector(".box10");
+const box11 = document.querySelector(".box11");
+const box12 = document.querySelector(".box12");
+const box13 = document.querySelector(".box13");
+const box14 = document.querySelector(".box14");
+const box15 = document.querySelector(".box15");
+const box16 = document.querySelector(".box16");
+const strictButton = document.querySelector("#strict");
+const onButton = document.querySelector("#on");
+const startButton = document.querySelector("#start");
+
+startButton.addEventListener("click", (event) => {
+  if (on == false || win) {
+    startButton.classList.add("on");
+    play();
+  }
+});
+
+startButton.addEventListener("click", (event) => {
+  if (on == true) {
+    startButton.classList.remove("on");
+    clearInterval(intervalId);
+    compTurn = false;
+    noneColor();
+    on = false;
+    turnCounter.innerHTML = '';
+  }
+});
+
+function play() {
+  win = false;
+  order = [];
+  playerOrder = [];
+  flash = 0;
+  intervalId = 0;
+  turn = 1;
+  turnCounter.innerHTML = 1;
+  good = true;
+  activeColor();
+  for (var i = 0; i < 50; i++) {
+    order.push(Math.floor(Math.random() * 16) + 1);
+  }
+  compTurn = true;
+
+  intervalId = setInterval(gameTurn, 800);
+}
+
+function gameTurn() {
+  on = false;
+
+  if (flash == turn) {
+    clearInterval(intervalId);
+    compTurn = false;
+    clearColor();
+    on = true;
+  }
+
+  if (compTurn) {
+    clearColor();
+    setTimeout(() => {
+      if (order[flash] == 1) one();
+      if (order[flash] == 2) two();
+      if (order[flash] == 3) three();
+      if (order[flash] == 4) four();
+      if (order[flash] == 5) five();
+      if (order[flash] == 6) six();
+      if (order[flash] == 7) seven();
+      if (order[flash] == 8) eight();
+      if (order[flash] == 9) nine();
+      if (order[flash] == 10) ten();
+      if (order[flash] == 11) eleven();
+      if (order[flash] == 12) twelve();
+      if (order[flash] == 13) thirTeen();
+      if (order[flash] == 14) fourTeen();
+      if (order[flash] == 15) fiveTeen();
+      if (order[flash] == 16) sixTeen();
+      flash++;
+    }, 200);
+  }
+}
+
+function activeColor() {
+  activeOne();
+  activeTwo();
+  activeThree();
+  activeFour();
+  activeFive();
+  activeSix();
+  activeSeven();
+  activeEight();
+  activeNine();
+  activeTen();
+  activeEleven();
+  activeTwelve();
+  activeThirTeen();
+  activeFourTeen();
+  activeFiveTeen();
+  activeSixTeen();
+}
+
+function noneColor() {
+  onenone();
+  twonone();
+  threenone();
+  fournone();
+  fivenone();
+  sixnone();
+  sevenone();
+  eightnone();
+  ninenone();
+  tenone();
+  elevenone();
+  twelvenone();
+  thirTeenone();
+  fourTeenone();
+  fivTeenone();
+  sixTeenone();
+}
+
+function clearColor() {
+  clearOne();
+  clearTwo();
+  clearThree();
+  clearFour();
+  clearFive();
+  clearSix();
+  clearSeven();
+  clearEight();
+  clearNine();
+  clearTen();
+  clearEleven();
+  clearTwelve();
+  clearThirTeen();
+  clearFourTeen();
+  clearFivTeen();
+  clearSixTeen();
+}
+
+function flashColor() {
+  flashOne();
+  flashTwo();
+  flashThree();
+  flashFour();
+  flashFive();
+  flashSix();
+  flashSeven();
+  flashEight();
+  flashNine();
+  flashTen();
+  flashEleven();
+  flashTewlve();
+  flashThirTeen();
+  flashFourTeen();
+  flashFivTeen();
+  flashSixTeen();
+
+}
+
+function check() {
+  if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) {
+    good = false;
+  }
+  if (playerOrder.length == 10 && good) {
+    winGame();
+  }
+
+  if (good == false) {
+    flashColor();
+    turnCounter.innerHTML = "NO!";
+   
+    
+    setTimeout(() => {
+      turnCounter.innerHTML = turn;
+      clearColor();
+
+
+      if (strict) {
+        play();
+      } else {
+        compTurn = true;
+        flash = 0;
+        playerOrder = [];
+        good = true;
+        intervalId = setInterval(gameTurn, 800);
+      }
+    }, 800);
+    
+    noise = false;
+  }
+
+  if (turn == playerOrder.length && good && !win) {
+    turn++;
+    playerOrder = [];
+    compTurn = true;
+    flash = 0;
+    turnCounter.innerHTML = turn;
+    intervalId = setInterval(gameTurn, 800);
+  }
+}
+
+function winGame() {
+  flashColor();
+  turnCounter.innerHTML = "WIN!";
+  on = false;
+  win = true;
+}
---
+++ Simon/js/pads.js
@@ -0,0 +1,648 @@
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////// * active pads function * //////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+
+function activeOne() {
+  box1.classList.add("actived1");
+}
+
+function activeTwo() {
+  box2.classList.add("actived2");
+}
+
+function activeThree() {
+  box3.classList.add("actived3");
+}
+
+function activeFour() {
+  box4.classList.add("actived4");
+}
+
+function activeFive() {
+  box5.classList.add("actived5");
+}
+
+function activeSix() {
+  box6.classList.add("actived6");
+}
+
+function activeSeven() {
+  box7.classList.add("actived7");
+}
+
+function activeEight() {
+  box8.classList.add("actived8");
+}
+
+function activeNine() {
+  box9.classList.add("actived9");
+}
+
+function activeTen() {
+  box10.classList.add("actived10");
+}
+
+function activeEleven() {
+  box11.classList.add("actived11");
+}
+
+function activeTwelve() {
+  box12.classList.add("actived12");
+}
+
+function activeThirTeen() {
+  box13.classList.add("actived13");
+}
+
+function activeFourTeen() {
+  box14.classList.add("actived14");
+}
+
+function activeFiveTeen() {
+  box15.classList.add("actived15");
+}
+
+function activeSixTeen() {
+  box16.classList.add("actived16");
+}
+
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////// * onclick pads function * //////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+
+function one() {
+  if (noise) {
+    let audio = document.getElementById("clip1");
+    audio.play();
+  }
+  noise = true;
+  box1.classList.remove("actived1");
+  box1.classList.add("clicked1");
+}
+
+function two() {
+  if (noise) {
+    let audio = document.getElementById("clip2");
+    audio.play();
+  }
+  noise = true;
+  box2.classList.add("clicked2");
+}
+
+function three() {
+  if (noise) {
+    let audio = document.getElementById("clip3");
+    audio.play();
+  }
+  noise = true;
+  box3.classList.add("clicked3");
+}
+
+function four() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box4.classList.add("clicked4");
+}
+
+function five() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box5.classList.add("clicked5");
+}
+
+function six() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box6.classList.add("clicked6");
+}
+
+function seven() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box7.classList.add("clicked7");
+}
+
+function eight() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box8.classList.add("clicked8");
+}
+
+function nine() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box9.classList.add("clicked9");
+}
+
+function ten() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box10.classList.add("clicked10");
+}
+
+function eleven() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box11.classList.add("clicked11");
+}
+
+function twelve() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box12.classList.add("clicked12");
+}
+
+function thirTeen() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box13.classList.add("clicked13");
+}
+
+function fourTeen() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box14.classList.add("clicked14");
+}
+
+function fiveTeen() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box15.classList.add("clicked15");
+}
+
+function sixTeen() {
+  if (noise) {
+    let audio = document.getElementById("clip4");
+    audio.play();
+  }
+  noise = true;
+  box16.classList.add("clicked16");
+}
+
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////// * inactive pads function * //////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+
+function onenone() {
+  box1.classList.remove("clicked1");
+  box1.classList.remove("actived1");
+}
+
+function twonone() {
+  box2.classList.remove("clicked2");
+  box2.classList.remove("actived2");
+}
+
+function threenone() {
+  box3.classList.remove("clicked3");
+  box3.classList.remove("actived3");
+}
+
+function fournone() {
+  box4.classList.remove("clicked4");
+  box4.classList.remove("actived4");
+}
+
+function fivenone() {
+  box5.classList.remove("clicked5");
+  box5.classList.remove("actived5");
+}
+
+function sixnone() {
+  box6.classList.remove("clicked6");
+  box6.classList.remove("actived6");
+}
+
+function sevenone() {
+  box7.classList.remove("clicked7");
+  box7.classList.remove("actived7");
+}
+
+function eightnone() {
+  box8.classList.remove("clicked8");
+  box8.classList.remove("actived8");
+}
+
+function ninenone() {
+  box9.classList.remove("clicked9");
+  box9.classList.remove("actived9");
+}
+
+function tenone() {
+  box10.classList.remove("clicked10");
+  box10.classList.remove("actived10");
+}
+
+function elevenone() {
+  box11.classList.remove("clicked11");
+  box11.classList.remove("actived11");
+}
+
+function twelvenone() {
+  box12.classList.remove("clicked12");
+  box12.classList.remove("actived12");
+}
+
+function thirTeenone() {
+  box13.classList.remove("clicked13");
+  box13.classList.remove("actived13");
+}
+
+function fourTeenone() {
+  box14.classList.remove("clicked14");
+  box14.classList.remove("actived14");
+}
+
+function fivTeenone() {
+  box15.classList.remove("clicked15");
+  box15.classList.remove("actived15");
+}
+
+function sixTeenone() {
+  box16.classList.remove("clicked16");
+  box16.classList.remove("actived16");
+}
+
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////// * clear pads function * //////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+
+function clearOne() {
+  box1.classList.remove("clicked1");
+}
+
+function clearTwo() {
+  box2.classList.remove("clicked2");
+}
+
+function clearThree() {
+  box3.classList.remove("clicked3");
+}
+
+function clearFour() {
+  box4.classList.remove("clicked4");
+}
+
+function clearFive() {
+  box5.classList.remove("clicked5");
+}
+
+function clearSix() {
+  box6.classList.remove("clicked6");
+}
+
+function clearSeven() {
+  box7.classList.remove("clicked7");
+}
+
+function clearEight() {
+  box8.classList.remove("clicked8");
+}
+
+function clearNine() {
+  box9.classList.remove("clicked9");
+}
+
+function clearTen() {
+  box10.classList.remove("clicked10");
+}
+
+function clearEleven() {
+  box11.classList.remove("clicked11");
+}
+
+function clearTwelve() {
+  box12.classList.remove("clicked12");
+}
+
+function clearThirTeen() {
+  box13.classList.remove("clicked13");
+}
+
+function clearFourTeen() {
+  box14.classList.remove("clicked14");
+}
+
+function clearFivTeen() {
+  box15.classList.remove("clicked15");
+}
+
+function clearSixTeen() {
+  box16.classList.remove("clicked16");
+}
+
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////// * eventListener onClick * //////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+
+box1.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(1);
+    check();
+    one();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box2.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(2);
+    check();
+    two();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box3.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(3);
+    check();
+    three();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box4.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(4);
+    check();
+    four();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box5.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(5);
+    check();
+    five();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box6.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(6);
+    check();
+    six();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box7.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(7);
+    check();
+    seven();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box8.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(8);
+    check();
+    eight();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box9.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(9);
+    check();
+    nine();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box10.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(10);
+    check();
+    ten();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box11.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(11);
+    check();
+    eleven();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box12.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(12);
+    check();
+    twelve();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box13.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(13);
+    check();
+    thirTeen();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box14.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(14);
+    check();
+    fourTeen();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box15.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(15);
+    check();
+    fiveTeen();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+box16.addEventListener("click", (event) => {
+  if (on) {
+    playerOrder.push(16);
+    check();
+    sixTeen();
+    if (!win) {
+      setTimeout(() => {
+        clearColor();
+      }, 300);
+    }
+  }
+});
+
+////////////////////////////////////////////////////////////// TODO//////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////// * eventListener onClick * //////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////// TODO //////////////////////////////////////////////////////////////////////
+
+function flashOne() {
+    box1.classList.add("clicked1");
+}
+
+function flashTwo() {
+    box2.classList.add("clicked2");
+}
+
+function flashThree() {
+  box3.classList.add("clicked3");
+}
+
+function flashFour() {
+    box4.classList.add("clicked4");
+}
+
+function flashFive() {
+    box5.classList.add("clicked5");
+}
+
+function flashSix() {
+    box6.classList.add("clicked6");
+}
+
+function flashSeven() {
+    box7.classList.add("clicked7");
+}
+
+function flashEight() {
+    box8.classList.add("clicked8");
+}
+
+function flashNine() {
+    box9.classList.add("clicked9");
+}
+
+function flashTen() {
+    box10.classList.add("clicked10");
+}
+
+function flashEleven() {
+    box11.classList.add("clicked11");
+}
+
+function flashTewlve() {
+    box12.classList.add("clicked12");
+}
+
+function flashThirTeen() {
+    box13.classList.add("clicked13");
+}
+
+function flashFourTeen() {
+    box14.classList.add("clicked14");
+}
+
+function flashFivTeen() {
+    box15.classList.add("clicked15");
+}
+
+function flashSixTeen() {
+    box16.classList.add("clicked16");
+}
