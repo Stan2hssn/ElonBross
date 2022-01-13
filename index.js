bfe9ffd7617f5050b02a4decd57a7983669d10a0
Stan2hssn <86244342+Stan2hssn@users.noreply.github.com>
2022-01-13 02:24:40 +0100

    HP v1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const cursor = document.querySelector(".cursor");
+const background = document.querySelector(".background");
+const fusee = document.querySelector(".background");
 const slider = document.querySelector(".items");
 
 let isDown = false;
@@ -6,10 +8,22 @@ let mousePosPct = undefined;
 
 const windowWidth = window.innerWidth;
 const itemWidth = slider.getBoundingClientRect().width;
+let before = [""];
+
+// var backgroundBefore = window.getComputedStyle(
+//     document.querySelector('.cursor'), ':before'
+// ).getPropertyValue('background-image');
+
+// before.push(backgroundBefore);
+
+// console.log(before);
 
 document.addEventListener("mousedown", (e) => {
   isDown = true;
-  cursor.classList.add('pushMouse');
+  cursor.classList.add("pushMouse");
+  cursor.style.transform = `translate(${e.clientX - 15}px, ${
+    e.clientY - 15
+  }px)`;
 });
 
 document.addEventListener("mouseleave", () => {
@@ -18,25 +32,52 @@ document.addEventListener("mouseleave", () => {
 
 document.addEventListener("mouseup", () => {
   isDown = false;
-  cursor.classList.remove('pushMouse');
-
+  cursor.classList.remove("pushMouse");
 });
 
 document.addEventListener("mousemove", (e) => {
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+
   if (!isDown) return;
 
-  mousePosPct = ((e.clientX - (windowWidth / 2)) / windowWidth) * 2;
+  mousePosPct = ((e.clientX - windowWidth / 2) / windowWidth) * 2;
 
-  console.log(mousePosPct)
+  console.log(mousePosPct);
 
-  render()
+  render();
 });
 
-document.addEventListener('wheel', e => {
-  const increment = 0.08
+background.addEventListener("mouseenter", () => {
+  cursor.style.backgroundImage = "url('cursor/svg/pointer_black.svg')";
+  // console.log(backgroundBefore);
+});
+
+background.addEventListener("mouseleave", () => {
+  cursor.style.backgroundImage = "url('cursor/pointer.svg')";
+});
+
+fusee.addEventListener("mouseenter", () => {
+  cursor.style.backgroundImage = "url('cursor/svg/pointer_black.svg')";
+});
+
+fusee.addEventListener("mouseleave", () => {
+  cursor.style.backgroundImage = "url('cursor/pointer.svg')";
+});
+
+
+document.addEventListener("wheel", (e) => {
+  const increment = 0.06;
+  cursor.classList.add("pushMouse");
 
   if (mousePosPct === undefined) mousePosPct = 0;
+
   if (mousePosPct > 0) mousePosPct = mousePosPct - 1;
+
+  if (e.deltaY) {
+    setTimeout(() => {
+        cursor.classList.remove("pushMouse");
+      }, 1000);
+  }
 
   if (e.deltaY > 0) {
     if (mousePosPct - increment > -1) {
@@ -45,7 +86,7 @@ document.addEventListener('wheel', e => {
       mousePosPct = -1;
     }
   } else {
-    console.log('up', increment)
+    console.log("up", increment);
     if (mousePosPct + increment < 0) {
       mousePosPct = mousePosPct + increment;
     } else {
@@ -54,7 +95,7 @@ document.addEventListener('wheel', e => {
   }
 
   render();
-})
+});
 
 function render() {
   const mousePosPx = mousePosPct * (itemWidth - windowWidth);
@@ -62,13 +103,10 @@ function render() {
   if (mousePosPct > 0) {
     slider.style.transform = `translateX(0px)`;
   } else if (mousePosPct < -1) {
-    slider.style.transform = `translateX(${itemWidth * -1 - windowWidth * -1}px)`;
+    slider.style.transform = `translateX(${
+      itemWidth * -1 - windowWidth * -1
+    }px)`;
   } else {
     slider.style.transform = `translateX(${mousePosPx}px)`;
   }
 }
-
-
-document.addEventListener("mousemove", (e) => {
-  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-});
